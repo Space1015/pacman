16b054819fb1a838956786993c16638e5b2c232d
Space1015 <charleswang1124@gmail.com>
2024-11-22 21:26:21 -0800

    y

--- src/Pacman.cpp
+++ src/Pacman.cpp
@@ -2,6 +2,6 @@
 Pacman::Pacman(){
     charTexture.loadFromFile("Resources/pacman.png");
     charSprite.setTexture(charTexture);
-    charSprite.setPosition(300,300);
+    charSprite.setPosition(32*13+16,32*19+16);
     States dir = States::STILL;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,18 @@
 #include <iostream>
 #include <GameMap.hpp>
 using namespace std;
-const unsigned speed = 88;
+const unsigned speed = 176;
 int main()
 {
     auto window = sf::RenderWindow({896u, 1152u}, "Pacman");
-    window.setFramerateLimit(144);
+    window.setFramerateLimit(60);
     GameMap gameMap;
     Pacman pacman;
     sf::Clock clock;
     sf::Time elapsed;
     while (window.isOpen())
     {
+        float deltaTime = clock.restart().asSeconds();
         for (auto event = sf::Event(); window.pollEvent(event);)
         {
             if (event.type == sf::Event::Closed)
@@ -21,7 +22,6 @@ int main()
                 window.close();
             }
         }
-        float deltaTime = clock.restart().asSeconds();
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
             pacman.dir = Pacman::States::UP;
         }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
@@ -32,13 +32,13 @@ int main()
             pacman.dir = Pacman::States::RIGHT;
         }
         if(pacman.dir == Pacman::States::UP){
-            pacman.charSprite.setPosition(pacman.charSprite.getPosition().x, pacman.charSprite.getPosition().y - speed * deltaTime);
+            pacman.charSprite.move(0, speed * deltaTime * -1);
         }else if(pacman.dir == Pacman::States::DOWN){
-            pacman.charSprite.setPosition(pacman.charSprite.getPosition().x, pacman.charSprite.getPosition().y + speed * deltaTime);
+            pacman.charSprite.move(0, speed * deltaTime);
         }else if(pacman.dir == Pacman::States::LEFT){
-            pacman.charSprite.setPosition(pacman.charSprite.getPosition().x - speed * deltaTime, pacman.charSprite.getPosition().y);
+            pacman.charSprite.move(speed * deltaTime * -1, 0);
         }else if(pacman.dir == Pacman::States::RIGHT){
-            pacman.charSprite.setPosition(pacman.charSprite.getPosition().x + speed * deltaTime, pacman.charSprite.getPosition().y);
+            pacman.charSprite.move(speed * deltaTime, 0);
         }
         window.clear();
         gameMap.displayMap(window, pacman);

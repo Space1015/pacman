17a55a57faa81c78d868836b908d19f80ca9707a
angelLi17 <liangelfremont@gmail.com>
2024-12-11 23:37:15 -0800

    win scene works :>

--- src/PelletManager.cpp
+++ src/PelletManager.cpp
@@ -18,10 +18,7 @@ PelletManager::PelletManager(GameMap gameMap) {
         }
     }
 }
-int getRemainingPellets() const
-{
-    return totalPellets;
-}
+
 int PelletManager::addScore(double x, double y){
     int ret = 0;
     if(fmod(x, 16) == 0 && fmod(y, 16) == 0){
